7db0b16efde01849f766d7d0ab1f4cb025396926
dst02030 <present@postech.ac.kr>
2022-03-16 10:18:28 +0900

    ch3_second & name change

--- Ch3_DiscoveringGroups/Generatefeedvector.py
+++ Ch3_DiscoveringGroups/Generatefeedvector.py
@@ -21,3 +21,49 @@ def getwordcounts(url):
 
     return d.feed.title, wc
         
+
+
+def getwords(html):
+    # Remove all the HTML tags
+    txt = re.compile(r'<[^>]+>').sub('', html)
+
+    # Split words by all non-alpha characters
+    words = re.compile(r'[^A-Z^a-z]+').split(txt)
+
+    # Convert to lowercase
+    return [word.lower() for word in words if word!='']
+
+
+apcount = {}
+wordcounts = {}
+
+for feedurl in file('feedlist.txt'):
+    title, wc = getwordcounts(feedurl)
+    wordcounts[title] = wc
+
+    for word,count in wc.items():
+        apcount.setdefault(word, 0)
+
+        if count>1:
+            apcount[word]+=1
+
+
+wordlist = []
+for w, bc in apcount.items():
+    frac = float(bc)/len(feedlist)
+    if frac > 0.1 and frac < 0.5: wordlist.append(w)
+
+
+out = file('blogdata.txt', 'w')
+out.write('Blog')
+
+for word in wordlist: out.write('\t%s' % word)
+out.write('\n')
+
+for blog, wc in wordcounts.items():
+    out.write(blog)
+
+    for word in wordlist:
+        if word in wc: out.write('\t%d' % wc[word])
+        else: out.write('\t0')
+    out.write('\n')

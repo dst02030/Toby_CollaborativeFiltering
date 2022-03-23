3d7116bb6c7a530cd2dd8085f8dbfd2dce5829f7
dst02030 <present@postech.ac.kr>
2022-03-23 10:05:06 +0900

    ch4_first

---
+++ Ch4_SearchingAndRanking/searchengine.py
@@ -0,0 +1,116 @@
+import urllib3
+from BeautifulSoup4 import *
+from urlparse import urljoin
+from pysqlite2 import dbapi2 as sqlite
+
+
+
+
+class crawler:
+    # Initialize the crawler with the name of database
+    def __init__(self, dbname):
+        self.con = sqlite.connect(dbname)
+
+    def __del__(self):
+        self.con.close()
+
+    def dbcommit(self):
+        self.con.commit()
+
+    
+
+    # Auxilliary function for getting an entry id and adding
+    # it if it's not present
+    def getentryid(self, table, field, value, createnew = True):
+        return None
+
+    # Index an individual page
+    def addtoindex(self, url, soup):
+        print(f"Indexing {url}")
+
+    # Extract the text from an HTML page (no tags)
+    def gettextonly(self, soup):
+        v = soup.string
+
+        if v == None:
+            c = soup.contents
+            resulttext = ''
+
+            for t in c:
+                subtext = self.gettextonly(t)
+                resulttext += subtext + '\n'
+
+            return resulttext
+
+        else:
+            return v.strip()
+
+    # Separate the words by any non-whitespace character
+    def separatewords(self, text):
+        splitter = re.compile('\\W*')
+        return [s.lower() for s in splitter.split(text) if s!= '']
+
+    # Return true if this url is already indexed
+    def isindexed(self, url):
+        return False
+
+    # Add a link between two pages
+    def addlinkref(self, urlFrom, urlTo, linkText):
+        pass
+
+    # Starting with a list of pages, do a breadth
+    # first search to the given depth, indexing pages
+    # as we go
+    def crawl(self, pages, depth = 2):
+        for i in range(depth):
+            newpages = set()
+
+            for page in pages:
+                try:
+                    c = urllib3.urlopen(page)
+
+                except:
+                    print(f"Could not open {page}")
+                    continue
+
+                soup = BeautifulSoup(c.read())
+                self.addtoindex(page, soup)
+
+                links = soup('a')
+
+                for link in links:
+                    if 'href' in dict(link.attrs):
+                        url = urljoin(page, link['href'])
+                        if url.find("'") != -1: continue
+
+                        url = url.split('#')[0] # Remove location portion
+
+                        if url[:4] == 'http' and not self.isindexed(url):
+                            newpages.add(url)
+
+                        linkText = self.gettextonly(link)
+                        self.addlinkref(page, url, linkText)
+
+
+                self.dbcommit()
+
+            pages = newpages
+
+    # Create the database tables
+    def createindextables(self):
+        self.con.execute('create table urllist(url)')
+        self.con.execute('create table wordlist(word)')
+        self.con.execute('create table wordlocation(urlid, wordid, location)')
+        self.con.execute('create table link(fromid integer, toid integer)')
+        self.con.execute('create table linkwords(wordid, linkid)')
+        self.con.execute('create index wordidx on wordlist(word)')
+        self.con.execute('create index urlidx on urllist(url)')
+        self.con.execute('create index wordurlidx on wordlocation(wordid)')
+        self.con.execute('create index urltoidx on link(toid)')
+        self.con.execute('create index urlfromidx on urlfromidx(fromid)')
+        self.dbcommit()
+        
+
+
+# Create a list of words to ignore
+ignorewords = set(['the', 'of', 'to', 'and', 'a', 'in', 'is', 'it'])
